3a1d7cf4d67b141133058cc5a69c5c85f7b7159b
Anders <anderssp1994@gmail.com>
2017-03-01 10:06:09 +0100

    Make network more stable and faster

--- src/network.c
+++ src/network.c
@@ -7,32 +7,57 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/queue.h>
+#include <time.h>
 #include <pthread.h>
 #include "dyad.h"
+#include "network.h"
+#include "utils.h"
 
 #define SIZE_BACKLOG 4
 #define MAX_MSG_SIZE 1024
 //Number of symbols in an IP address including dots
 #define SIZE_IP 15
+#define DYAD_TIMEOUT 1
 
-STAILQ_HEAD(stailhead, entry) messages_head = STAILQ_HEAD_INITIALIZER(messages_head);
-
-struct entry {
+typedef struct Message_queue_node_t {
     char message[MAX_MSG_SIZE];
-    STAILQ_ENTRY(entry) entries;
-};
+    size_t length;
+    STAILQ_ENTRY(Message_queue_node_t) messages;
+} Message_queue_node_t;
+
+STAILQ_HEAD(messages_queue_head, Message_queue_node_t);
+
+static struct messages_queue_head received_messages_head;
+static struct messages_queue_head send_messages_head;
 
 pthread_mutex_t msg_mutex;
 pthread_mutex_t stream_mutex;
 
-void addMessage(char* message) {
-    pthread_mutex_lock(&msg_mutex);
-    struct entry* en;
-    en = malloc(sizeof(struct entry));
-    if (en) {
-        strcpy(en->message, message);
+void addMessageToQueue(struct messages_queue_head * queue, char* message, size_t length) {
+    if (length > MAX_MSG_SIZE) {
+        fprintf(stderr, "Received too long message. Discarding. %d > %d\n", (int)length, MAX_MSG_SIZE);
+        printf("Message is: %s\n", message);
+        return;
     }
-    STAILQ_INSERT_TAIL(&messages_head, en, entries);
+    /*
+    printf("Received message %s\n", message);
+    printf("Adding to queue\n");
+    */
+    pthread_mutex_lock(&msg_mutex);
+    Message_queue_node_t* node;
+    node = malloc(sizeof(Message_queue_node_t));
+    if (node) {
+        memcpy(node->message, message, length);
+        node->length = length;
+    }
+    STAILQ_INSERT_TAIL(queue, node, messages);
+    /*
+    printf("Messages in queue: \n");
+    STAILQ_FOREACH(en, &messages_head, entries) {
+        printf("%s\n", en->message);
+    }
+    printf("End messages in queue. \n");
+    */
     pthread_mutex_unlock(&msg_mutex);
 }
 
@@ -65,9 +90,20 @@ static void removeStreamFromList(dyad_Stream* s) {
 }
 
 static void onData(dyad_Event* e) {
-    //printf("Received data from %s:%d: %s\n", dyad_getAddress(e->stream),
-    //       dyad_getPort(e->stream), e->data);
-    addMessage(e->data);
+    /*
+    printf("Received data from %s:%d: %s\n", dyad_getAddress(e->stream),
+           dyad_getPort(e->stream), e->data);
+   */
+    /*
+    printf("Received data from %s:%d:", dyad_getAddress(e->stream), dyad_getPort(e->stream));
+    int i;
+    for ( i = 0; i < e->size; ++i) {
+        putc(e->data[i], stdout);
+    }
+    putc('\n',stdout);
+    printf("Message length: %d\n", e->size);
+    */
+    addMessageToQueue(&received_messages_head, e->data, e->size);
 }
 
 static void onClose(dyad_Event* e) {
@@ -80,51 +116,49 @@ static void onConnect(dyad_Event* e) {
     printf("Connected to %s:%d\n", dyad_getAddress(e->stream), dyad_getPort(e->stream));
 }
 
-
+static void onError(dyad_Event* e) {
+    printf("Dyad error: %s\n", e->data);
+    exit(1);
+}
 
 static void onAccept(dyad_Event* e) {
     addStreamToList(e->remote);
     printf("Accepted connection from %s:%d\n",dyad_getAddress(e->remote), dyad_getPort(e->remote));
     dyad_addListener(e->remote, DYAD_EVENT_CLOSE, onClose, NULL);
     dyad_addListener(e->remote, DYAD_EVENT_DATA, onData, NULL);
+    dyad_addListener(e->remote, DYAD_EVENT_ERROR, onError, NULL);
 }  
 
 
+static void popAndBroadcast(struct messages_queue_head * queue) {
+    pthread_mutex_lock(&msg_mutex);
+    if (STAILQ_EMPTY(queue)) {
+        pthread_mutex_unlock(&msg_mutex);
+        return;
+    }
+    Message_queue_node_t* node = STAILQ_FIRST(&send_messages_head);
+    int i;
+    for (i = 0; i < num_connected_streams; ++i) {
+        dyad_write(stream_list[i], node->message, node->length);
+    }
+    STAILQ_REMOVE_HEAD(queue, messages);
+    pthread_mutex_unlock(&msg_mutex);
+    free(node);
+}
+
 static void* workerThread() {
     while (true) {
-        //stateMachine();
-        sleep(1);
+        popAndBroadcast(&send_messages_head);
         dyad_update();
-        //broadcastTest("Hello world");
-        printf("Number of connected streams: %d\n", num_connected_streams);
-        int i;
-        for ( i = 0; i < num_connected_streams; ++i) {
-            printf("Connected to: %s:%d\n",
-                dyad_getAddress(stream_list[i]), dyad_getPort(stream_list[i]));
-        }
-        /*
-        printf("Messages in queue\n");
-        struct entry* en;
-        STAILQ_FOREACH(en, &messages_head, entries)
-            printf("%s\n", en->message);
-        */
+        //Sleep a bit so that the process doesn't consume too much cpu time
+        usleep(1);
     }
-    //printf("No streams left. Shutting down\n");
     dyad_shutdown();
     pthread_exit(NULL);
     return NULL;
 }
 
-void net_listen(char* my_hostname, uint16_t my_port) {
-    printf("Listening for connections to %s:%d ...\n", my_hostname, my_port);
-    dyad_Stream* s = dyad_newStream();
-    dyad_listenEx(s, my_hostname, my_port, SIZE_BACKLOG);
-    dyad_addListener(s, DYAD_EVENT_ACCEPT, onAccept, NULL);
-    dyad_addListener(s, DYAD_EVENT_DATA, onData, NULL);
-    return;
-}
-
-void net_init(char* my_hostname, uint16_t my_port) {
+void net_init(char* ips_and_ports[]) {
     //Init mutexes
     if (pthread_mutex_init(&msg_mutex, NULL) != 0)
     {
@@ -137,11 +171,25 @@ void net_init(char* my_hostname, uint16_t my_port) {
         exit(1);
     }
     //Init messagequeue
-    STAILQ_INIT(&messages_head);
+    STAILQ_INIT(&received_messages_head);
+    STAILQ_INIT(&send_messages_head);
     //Init dyad
     dyad_init();
     dyad_setUpdateTimeout(0);
+    //Connect to other elevators
+    int i = 1;
+    while ( i < NUM_ELEVATORS ) {
+        char* hostname = ips_and_ports[2*i];
+        uint16_t port = str2int(ips_and_ports[2*i+1]);
+        net_connect(hostname, port);
+        ++i;
+    }
+    //Listen for incoming connections
+    char* my_hostname = ips_and_ports[0];
+    int my_port = str2int(ips_and_ports[1]);
     net_listen(my_hostname, my_port);
+
+    //Start worker thread
     pthread_t thread_dyad;
     pthread_create(&thread_dyad, NULL, workerThread, NULL);
 }
@@ -150,49 +198,64 @@ void net_init(char* my_hostname, uint16_t my_port) {
 
 static int tryConnect(dyad_Stream* s, char* hostname, int port) {
     dyad_connect(s, hostname, port);
-    usleep(250000);
-    //We must update manually because the dyad worker thread may not be running
-    dyad_update();
-    if (dyad_getState(s) == DYAD_STATE_CONNECTED) {
-        return 0;
+    clock_t connect_time = 0.5*CLOCKS_PER_SEC; //clock cycles
+    clock_t start_time = clock();
+    clock_t diff = 0;
+    while ( diff < connect_time) {
+        //We must update manually because the dyad worker thread may not be running
+        dyad_update();
+        if (dyad_getState(s) == DYAD_STATE_CONNECTED) {
+            return 0;
+        }
+        diff = clock() - start_time;
     }
     return -1;
+}
+
+void net_listen(char* my_hostname, uint16_t my_port) {
+    printf("Listening for connections to %s:%d ...\n", my_hostname, my_port);
+    dyad_Stream* s = dyad_newStream();
+    dyad_listenEx(s, my_hostname, my_port, SIZE_BACKLOG);
+    dyad_addListener(s, DYAD_EVENT_ACCEPT, onAccept, NULL);
+    dyad_addListener(s, DYAD_EVENT_DATA, onData, NULL);
+    dyad_addListener(s, DYAD_EVENT_ERROR, onError, NULL);
 }
 
 void net_connect(char* hostname, uint16_t port) {
     dyad_Stream* s = dyad_newStream();
     dyad_addListener(s, DYAD_EVENT_CONNECT, onConnect, NULL);
-    dyad_addListener(s, DYAD_EVENT_CLOSE, onClose, NULL);
-    dyad_addListener(s, DYAD_EVENT_DATA, onData, NULL);
+    dyad_addListener(s, DYAD_EVENT_ERROR, onError, NULL);
     printf("Trying to connect to %s:%d ...\n", hostname, port);
     int ret = tryConnect(s, hostname, port);
     if (ret) {
         printf("No server found\n");
+        dyad_close(s);
     }
     else {
         printf("Server found\n");
+        dyad_addListener(s, DYAD_EVENT_CLOSE, onClose, NULL);
+        dyad_addListener(s, DYAD_EVENT_DATA, onData, NULL);
     }
 }
 
 void net_broadcast(char* data, size_t length) {
     //printf("Broadcasting message: %s\n", data);
-    int i;
-    for (i = 0; i < num_connected_streams; ++i) {
-        dyad_write(stream_list[i], data, length);
-    }
+    //printf("Broadcasting...\n");
+    addMessageToQueue(&send_messages_head, data, length);
 }
 
-int net_getMessage(char* target) {
-    if (STAILQ_EMPTY(&messages_head)) {
+int net_getMessage(char* target, size_t* received_msg_length) {
+    pthread_mutex_lock(&msg_mutex);
+    if (STAILQ_EMPTY(&received_messages_head)) {
+        pthread_mutex_unlock(&msg_mutex);
         return -1;
     }
-    pthread_mutex_lock(&msg_mutex);
-    struct entry* en = STAILQ_FIRST(&messages_head)->message;
-    char* msg = en->message;
-    strcpy(target, msg);
-    STAILQ_REMOVE_HEAD(&messages_head, entries);
-    free(en);
+    Message_queue_node_t* node = STAILQ_FIRST(&received_messages_head);
+    memcpy(target, node->message, node->length);
+    STAILQ_REMOVE_HEAD(&received_messages_head, messages);
+    free(node);
     pthread_mutex_unlock(&msg_mutex);
+    *received_msg_length = node->length;
     return 0;
 }
 
