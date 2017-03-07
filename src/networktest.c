5e59eba5df8936769e1eab5b3abb8b050036f5ec
MHmorgan <magnus.hirt@gmail.com>
2017-03-07 12:53:49 +0100

    Merged from Anders

--- src/networktest.c
+++ src/networktest.c
@@ -11,38 +11,38 @@
 
 int str2int(char* str) {
     int sum = 0;
-    int i = 0;
+    int i   = 0;
     //First find end of string
-    while (str[i+1] != '\0') {
+    while (str[i + 1] != '\0') {
         ++i;
     }
     int exp = 0;
-    for (; i>=0; --i) {
-        sum += (str[i] - '0' )*pow(10, exp++);
+    for (; i >= 0; --i) {
+        sum += (str[i] - '0') * pow(10, exp++);
     }
     return sum;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != (2*NUM_ELEVATORS + 1)) {
-        printf("Invalid number of arguments. Expected %d\n", 2*NUM_ELEVATORS);
+    if (argc != (2 * NUM_ELEVATORS + 1)) {
+        printf("Invalid number of arguments. Expected %d\n", 2 * NUM_ELEVATORS);
         exit(1);
     }
     char* my_hostname = argv[1];
-    int my_port = str2int(argv[2]);
+    int   my_port     = str2int(argv[2]);
     char* hostname;
-    int port;
-    int i = 3;
+    int   port;
+    int   i = 3;
     while ( i < argc ) {
         hostname = argv[i++];
-        port = str2int(argv[i++]);
+        port     = str2int(argv[i++]);
         net_connect(hostname, port);
     }
     net_init(my_hostname, my_port);
     //net_listen(my_hostname, my_port);
     char receivedMsg[1024];
     char sendMsg[1024];
-    while(true) {
+    while (true) {
         sleep(1);
         sprintf(sendMsg, "Hello from %s:%d. The time is: %d\n",
                 my_hostname, my_port, (int)time(NULL));
@@ -55,4 +55,4 @@ int main(int argc, char* argv[]) {
         //}
     }
     return 0;
-}
+} /* main */
