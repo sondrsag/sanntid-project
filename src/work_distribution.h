60ffb2375198b26faf8fb92145777d38f210b5ab
MHmorgan <magnus.hirth@gmail.com>
2017-03-14 17:58:26 +0100

    Started writing finished job light handling

--- src/work_distribution.h
+++ src/work_distribution.h
@@ -6,16 +6,17 @@
 //#include "network.h"
 
 typedef void (*HandleJobCallback_t)(Job_t);
+typedef void (*AlertJobFinished_t)(Job_t);
 
 void work_distribution_start(HandleJobCallback_t jobCallback, int IdLocalElevator); //used by main
 
 /* functions for local elevator module*/
-void wd_updateLocalElevStatus(ElevatorStatus_t new_status); 
-void wd_receiveJob_from_local_elevator(Job_t job); 
+void wd_updateLocalElevStatus(ElevatorStatus_t new_status);
+void wd_receiveJob_from_local_elevator(Job_t job);
 
 /*function for communication module*/
 void wd_receiveJob_from_elcom(Job_t job);
-void wd_receiveCallsListFromPrimary(OutsideCallsList_t newOutsideCallsList); 
+void wd_receiveCallsListFromPrimary(OutsideCallsList_t newOutsideCallsList);
 void wd_HandleInternalCallsAfterRestart(InternalCallsList_t newInternalCalls);
 void wd_updateElevStatus(ElevatorStatus_t new_status, int assignee_id);
 
