b67b08918ae53c3bb8c6ef4879fe701149538fec
MHmorgan <mhmorgan42@gmail.com>
2017-03-14 23:42:03 +0100

    Edited job validation

--- src/elevatorcontrol.c
+++ src/elevatorcontrol.c
@@ -243,7 +243,7 @@ bool validJob(Job_t job)
             ret         = false;
         } else if (jobs[i].floor == job.floor && jobs[i].button == job.button) {
             ret = false;
-        } else if (i == 1 && jobs[i - 1]floor == job.floor &&
+        } else if (i == 1 && jobs[i - 1].floor == job.floor &&
                    jobs[i - 1].button == job.button) {
             ret = false;
         }
