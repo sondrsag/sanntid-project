d0b8844149b73d695de6a21c0e471b042c4cd8c2
MHmorgan <mhmorgan42@gmail.com>
2017-03-15 03:11:18 +0100

    Updated code esthetics
    Placed arguments on multiple lines,  moved som brackets and removed some empty lines.

--- src/elcom.c
+++ src/elcom.c
@@ -19,25 +19,26 @@ static void printMessage(char* message, size_t length) {
         putc(message[i], stdout);
     }
     putc('\n', stdout);
-
 }
 
 void printJob(Job_t job) {
     printf(("Floor: %d\n"
             "Button: %d\n"
             "Finished?: %d\n"
-            "Assigned to: %d\n"), job.floor, job.button, job.finished, job.assignee);
+            "Assigned to: %d\n"),
+           job.floor, job.button, job.finished, job.assignee);
 }
 
 void printElevatorStatus(ElevatorStatus_t status) {
     printf(("Working: %d\n"
-             "Finished?: %d\n"
-             "Current floor: %d\n"
-             "Next floor: %d\n"
-             "Availble: %d\n"
-             "Action: %d\n"
-             "Direction: %d\n"), status.working, status.finished, status.current_floor,
-             status.next_floor, status.available, status.action, status.direction);
+            "Finished?: %d\n"
+            "Current floor: %d\n"
+            "Next floor: %d\n"
+            "Availble: %d\n"
+            "Action: %d\n"
+            "Direction: %d\n"),
+           status.working, status.finished, status.current_floor,
+           status.next_floor, status.available, status.action, status.direction);
 }
 
 void printOutsideCallsList(OutsideCallsList_t list) {
@@ -47,7 +48,7 @@ void printOutsideCallsList(OutsideCallsList_t list) {
                 "Down: %d\n"
                 "Id up: %d\n"
                 "Id down: %d\n"), list[i].up, list[i].down,
-                list[i].el_id_up, list[i].el_id_down);
+               list[i].el_id_up, list[i].el_id_down);
     }
 }
 
@@ -62,11 +63,12 @@ void printInternalCallsList(InternalCallsList_t list) {
 }
 
 static void deserializeMessageAndDistribute(Message_type_t message_type,
-            char* message, size_t message_length, int sender_id) {
-
+                                            char*          message,
+                                            size_t         message_length,
+                                            int            sender_id) {
     if (message_type == JOB) {
         Job_t job;
-        int ret = de_serialize_job_from_buffer(message, &job);
+        int   ret = de_serialize_job_from_buffer(message, &job);
         if (ret == -1) {
             fprintf(stderr, ("Could not deserialize message into Job."
                              "Malformed or missing data. Discarding"));
@@ -75,11 +77,10 @@ static void deserializeMessageAndDistribute(Message_type_t message_type,
             return;
         }
         wd_receiveJob_from_elcom(job);
-        
     }
     else if (message_type == ELEVATOR_STATUS) {
         ElevatorStatus_t status;
-        int ret = de_serialize_ElevatorStatus_from_buffer(message, &status);
+        int              ret = de_serialize_ElevatorStatus_from_buffer(message, &status);
         if (ret == -1) {
             fprintf(stderr, ("Could not deserialize message into ElevatorStatus."
                              "Malformed or missing data. Discarding"));
@@ -87,12 +88,12 @@ static void deserializeMessageAndDistribute(Message_type_t message_type,
             printMessage(message, message_length);
             return;
         }
-        
+
         wd_updateElevStatus(status, sender_id);
-        
     }
     else if (message_type == OUTSIDE_CALLS) {
         OutsideCallsList_t list;
+
         int ret = de_serialize_OutsideCallsList_from_buffer(message, list);
         if (ret == -1) {
             fprintf(stderr, ("Could not deserialize message into external calls list."
@@ -101,15 +102,14 @@ static void deserializeMessageAndDistribute(Message_type_t message_type,
             printMessage(message, message_length);
             return;
         }
-        
-		if(sender_id == net_getMasterId()) 
-		{
-			wd_receiveCallsListFromPrimary(list);
-		}
-		
+
+        if (sender_id == net_getMasterId()) {
+            wd_receiveCallsListFromPrimary(list);
+        }
     }
     else if (message_type == INTERNAL_CALLS) {
         InternalCallsList_t calls_list;
+
         int ret = deserializeInternalCallsListFromBuffer(message, calls_list);
         if (ret) {
             fprintf(stderr, ("Could not deserialize internal calls list message."
@@ -118,18 +118,16 @@ static void deserializeMessageAndDistribute(Message_type_t message_type,
             printMessage(message, message_length);
             return;
         }
-        if(elcom_just_started == true)
-		{
-			wd_HandleInternalCallsAfterRestart(calls_list);
-			elcom_just_started = false;
-		}
-			
-    }
-    else {
+
+        if (elcom_just_started == true) {
+            wd_HandleInternalCallsAfterRestart(calls_list);
+            elcom_just_started = false;
+        }
+    } else {
         fprintf(stderr, "Unable to interpret message, discarding.\nMessage was: ");
         printMessage(message, message_length);
     }
-}
+} /* deserializeMessageAndDistribute */
 
 static void* workerThread() {
     char received_msg[1024];
@@ -137,31 +135,30 @@ static void* workerThread() {
     size_t received_msg_length;
 
     int sender_id;
-	
-	elcom_just_started = true;
-	
-	
+
+    elcom_just_started = true;
+
+
     sleep(2);
-    while(true) {
+    while (true) {
         usleep(1000);
         while (net_getMessage(received_msg, &received_msg_length, &sender_id) == 0) {
-            //printf("Received message from %d: ", sender_id);
-            //printMessage(received_msg, received_msg_length);
-
             if (received_msg_length != MESSAGE_LENGTH) {
                 fprintf(stderr, ("ERROR: Received message of invalid length %zd!=%d. "
                                  "Too lazy to handle. Discarding.\n"),
-                                 received_msg_length, MESSAGE_LENGTH);
+                        received_msg_length, MESSAGE_LENGTH);
                 continue;
             }
 
             Message_type_t message_type = identify_message_type(received_msg);
-
-            deserializeMessageAndDistribute(message_type, received_msg, received_msg_length, sender_id);
+            deserializeMessageAndDistribute(message_type,
+                                            received_msg,
+                                            received_msg_length,
+                                            sender_id);
         }
     }
     return NULL;
-}
+} /* workerThread */
 
 
 void elcom_broadcastJob(Job_t job) {
@@ -174,16 +171,16 @@ void elcom_broadcastOutsideCallsList(OutsideCallsList_t outside_calls_list) {
     char buf[MESSAGE_LENGTH] = {0};
 
     serialize_OutsideCallsList_into_buffer(outside_calls_list,
-        sizeof(OutsideCallsList_t), buf, sizeof(buf));
+                                           sizeof(OutsideCallsList_t),
+                                           buf,
+                                           sizeof(buf));
 
     net_broadcast(buf, sizeof(buf));
 }
 
 void elcom_broadcastInternalCallsList(InternalCallsList_t calls_list) {
     char buf[MESSAGE_LENGTH] = {0};
-
     serializeInternalCallsListIntoBuffer(calls_list, buf, sizeof(buf));
-
     net_broadcast(buf, sizeof(buf));
 }
 
@@ -205,4 +202,3 @@ void elcom_init(unsigned int const my_id) {
     pthread_t thread_elcom;
     pthread_create(&thread_elcom, NULL, workerThread, NULL);
 }
-
